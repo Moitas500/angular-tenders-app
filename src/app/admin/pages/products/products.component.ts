c51eaddafc6f3272d7bc8fcdb08ae88787de47e1
Moitas500 <camilofour1@gmail.com>
2024-05-22 11:44:03 -0500

    Admin products
    Add list products and create product

--- src/app/admin/pages/products/products.component.ts
+++ src/app/admin/pages/products/products.component.ts
@@ -1,15 +1,53 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, WritableSignal, computed, inject, signal } from '@angular/core';
 import { ProductsService } from '../../services/products-service.service';
+import { Product } from '../../interfaces/product.interface';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export default class ProductsComponent {
+export default class ProductsComponent implements OnInit{
 
+  public productForm!: FormGroup
+  public products = computed(() => this._products())
   private _productsService = inject(ProductsService)
+  private _products: WritableSignal<Product []> = signal([])
+  private fb = inject(FormBuilder)
+
+  ngOnInit(): void {
+    this.updateProductList()
+
+    this.productForm = this.fb.group({
+      id: ['', Validators.required],
+      description: ['', Validators.required],
+      name: ['', Validators.required],
+      price: [0, Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if ( !this.productForm.valid ) {
+      alert('Rellene todos los campos')
+      return
+    }
+
+    this._productsService.createProduct(this.productForm.value).subscribe({
+      next: () => {
+        this.updateProductList()
+      }
+    })
+  }
+
+  updateProductList() {
+    this._productsService.getProductsList().subscribe( products => {
+      this._products.set(products)
+     })
+  }
 
 }
--- src/app/admin/services/products-service.service.ts
+++ src/app/admin/services/products-service.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { HeaderService } from '../../shared/services/header.service';
+import { environment } from '../../../environment/environment.local';
+import { Product } from '../interfaces/product.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  private _http = inject(HttpClient)
+  private _headers = inject(HeaderService)
+  private _baseUrl = environment.serviceHost
+
+  getProductsList() {
+    return this._http.get<Product []>(`${this._baseUrl}/products`, {
+      headers: this._headers.getHeaders()
+    })
+  }
+
+  createProduct(product: Product) {
+    return this._http.post(`${this._baseUrl}/products/create-product`, product, {
+      headers: this._headers.getHeaders()
+    })
+  }
+
 }
